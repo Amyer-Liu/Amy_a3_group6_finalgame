b98d36e79fc26658530acc8c081efe1579d2cca8
Amyer-Liu <a383liu@uwaterloo.ca>
2026-07-14 17:07:29 -0400

    start screen fixed

--- start_screen.js
+++ start_screen.js
@@ -1,12 +1,50 @@
+// Start screen "PRESS START" hit area (as fractions of canvas size,
+// so it stays aligned no matter how startBg gets stretched)
+let startButtonBox = {
+  xFrac: 0.5,
+  yFrac: 0.905,
+  wFrac: 0.19,
+  hFrac: 0.075
+};
+
+let DEBUG_START_BUTTON = false; // flip true to visualize the click zone
+
 function drawStartScreen() {
   imageMode(CORNER);
   image(startBg, 0, 0, width, height);
 
-  image(titleImg, width/2 - 450, height/2 - 350, 900, 300);
-
-  drawButton("Start", width/2, 400, 320, 64, startBtnPressed);
-
-  animateUpTest();
-  drawUpAnimation(420, 470);
+  drawStartButtonOverlay();
 }
 
+function getStartButtonRect() {
+  let bw = width * startButtonBox.wFrac;
+  let bh = height * startButtonBox.hFrac;
+  let bx = width * startButtonBox.xFrac;
+  let by = height * startButtonBox.yFrac;
+  return { bx, by, bw, bh };
+}
+
+function drawStartButtonOverlay() {
+  let { bx, by, bw, bh } = getStartButtonRect();
+
+  let hover = mouseX > bx - bw/2 && mouseX < bx + bw/2 &&
+              mouseY > by - bh/2 && mouseY < by + bh/2;
+
+  if (hover) {
+    cursor(HAND);
+    noStroke();
+    fill(255, 255, 255, 40);
+    rect(bx - bw/2, by - bh/2, bw, bh, 10);
+  } else {
+    cursor(ARROW);
+  }
+
+  if (DEBUG_START_BUTTON) {
+    push();
+    noFill();
+    stroke(255, 0, 0);
+    strokeWeight(2);
+    rect(bx - bw/2, by - bh/2, bw, bh);
+    pop();
+  }
+}
